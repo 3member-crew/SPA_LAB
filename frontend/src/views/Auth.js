f365d8f9b2b0f54f86afa4578117e7c4f177fbfe
saldaev vladimir <caldaevvlad3@gmail.com>
2024-01-05 22:53:03 +1000

    fixed onClick events

--- frontend/src/views/Auth.js
+++ frontend/src/views/Auth.js
@@ -2,29 +2,30 @@ import '../App.css';
 import { faVk, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Auth() {
+
+function onLoginClick() {
+    const container = document.getElementById('container');
+    const loginBtn = document.getElementById('login');
+    container.classList.remove("active");
+};
+
+function onRegisterClick() {
     const container = document.getElementById('container');
     const registerBtn = document.getElementById('register');
-    const loginBtn = document.getElementById('login');
+    container.classList.add("active");
+};
 
-    registerBtn.addEventListener('click', () => {
-        container.classList.add("active");
-    });
-
-    loginBtn.addEventListener('click', () => {
-        container.classList.remove("active");
-    });
-
+function Auth() {
     return(
-        <div class="container" id="container">
-            <div class="form-container sign-up">
+        <div className="container" id="container">
+            <div className="form-container sign-up">
                 <form>
                     <h1>Создайте аккаунт</h1>
-                    <div class="social-icons">
-                        <a href="#" class="icon">
+                    <div className="social-icons">
+                        <a href="#" className="icon">
                             <FontAwesomeIcon icon={faVk} />
                         </a>
-                        <a href="#" class="icon">
+                        <a href="#" className="icon">
                             <FontAwesomeIcon icon={faGoogle} />
                         </a>
                     </div>
@@ -37,14 +38,14 @@ function Auth() {
                     <button>Создать</button>
                 </form>
             </div>
-            <div class="form-container sign-in">
+            <div className="form-container sign-in">
                 <form>
                     <h1>Войти в аккаунт</h1>
-                    <div class="social-icons">
-                        <a href="#" class="icon">
+                    <div className="social-icons">
+                        <a href="#" className="icon">
                             <FontAwesomeIcon icon={faVk} />
                         </a>
-                        <a href="#" class="icon">
+                        <a href="#" className="icon">
                             <FontAwesomeIcon icon={faGoogle} />
                         </a>
                     </div>
@@ -57,19 +58,19 @@ function Auth() {
                     <button>Войти</button>
                 </form>
             </div>
-            <div class="toggle-container">
-                <div class="toggle">
-                    <div class="toggle-panel toggle-left">
+            <div className="toggle-container">
+                <div className="toggle">
+                    <div className="toggle-panel toggle-left">
                         <h1>С возвращением!</h1>
                         <p>Введите свои персональные данные, чтобы пользоваться полным функционалом сайта</p>
-                        <button class="hidden" id="login">
+                        <button className="hidden" id="login" onClick={onLoginClick}>
                             Войти
                         </button>
                     </div>
-                    <div class="toggle-panel toggle-right">
+                    <div className="toggle-panel toggle-right">
                         <h1>Привет, друг!</h1>
                         <p>Зарегистрируйтесь, введя свои персональные данные</p>
-                        <button class="hidden" id="register">
+                        <button className="hidden" id="register" onClick={onRegisterClick}>
                             Создать
                         </button>
                     </div>
