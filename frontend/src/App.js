6a01df3234efa165140f5224cde8a000224352d7
Evgenii Isaenko <isaenko.ea.work@gmail.com>
2024-01-06 23:15:09 +1000

    added basic home page and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Auth from './views/Auth';
+import Home from './views/Home';
+import Layout from './'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Auth />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
