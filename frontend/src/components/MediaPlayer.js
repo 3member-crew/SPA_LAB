b8490941002f340335964de12d67f9f71da03285
saldaev vladimir <caldaevvlad3@gmail.com>
2024-01-06 19:18:55 +1000

    added url input in room

--- frontend/src/components/MediaPlayer.js
+++ frontend/src/components/MediaPlayer.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player/lazy';
 
 function MediaPlayer() {
+    const [url, setUrl] = useState('https://youtu.be/E5gvllH2fag');
+
+    const HandleUrlChange = (event) => {
+        setUrl(event.target.value);
+    };
+
     return (
-        <ReactPlayer 
-            url='https://youtu.be/E5gvllH2fag' 
-        />
+        <>
+            <input type="text" value={url} onChange={HandleUrlChange} />
+            <ReactPlayer
+                url={url}
+            />
+        </>
     )
 }
 
