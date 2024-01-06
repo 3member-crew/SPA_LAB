c501ea2e45b2d91e2f40c1ee163ee7cb2b43cb29
saldaev vladimir <caldaevvlad3@gmail.com>
2024-01-07 03:51:17 +1000

    pseudo-merged frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
 import Auth from './views/Auth';
-import JoinRoom from './views/JoinRoom';
-import Room from './views/Room';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import Home from './views/Home';
+import Layout from './'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<Auth />} />
-                    <Route exact path="/home" element={<JoinRoom />} />
-                    <Route exact path="/room" element={<Room />} />
-                </Routes> 
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
