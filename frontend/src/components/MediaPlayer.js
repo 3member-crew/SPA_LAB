42f0faaad111daaac01d0a978a0be3e24c310ab6
saldaev vladimir <caldaevvlad3@gmail.com>
2024-01-07 22:46:48 +1000

    added video events handlers (play + pause)

--- frontend/src/components/MediaPlayer.js
+++ frontend/src/components/MediaPlayer.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player/lazy';
 
-function MediaPlayer() {
+function MediaPlayer({ onPlay, onPause, onSeek }) {
     const [url, setUrl] = useState('https://youtu.be/E5gvllH2fag');
 
     const HandleUrlChange = (event) => {
         setUrl(event.target.value);
     };
 
+    const handlePlay = () => {
+        console.log("play - inner");
+        if (onPlay) {
+            onPlay();
+        }
+    };
+    
+    const handlePause = () => {
+        console.log("pause - inner");
+        if (onPause) {
+            onPause();
+        }
+    };
+    
+    const handleSeek = () => {
+        console.log("seek - inner");
+        if (onSeek) {
+            onSeek();
+        }
+    }
+
     return (
         <>
             <input type="text" value={url} onChange={HandleUrlChange} />
             <ReactPlayer
                 url={url}
+                onPlay={handlePlay}
+                onPause={handlePause}
+                onSeek={handleSeek}
             />
         </>
     )
