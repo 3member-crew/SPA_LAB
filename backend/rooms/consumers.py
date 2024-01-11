6e5b7ebaa591c983693a21597eaa2cf4e313ba91
ky0ufu <superjar@mail.ru>
2024-01-12 03:24:54 +1000

    add ws chat

--- backend/rooms/consumers.py
+++ backend/rooms/consumers.py
@@ -1,24 +1,37 @@
 import json 
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import Video, Room 
+from .models import Video, Room, Member
+from .serializers import MembersSerializer
 from asgiref.sync import sync_to_async 
 from rest_framework.authtoken.models import Token
 from channels.db import database_sync_to_async
+
+@database_sync_to_async
+def get_user(self, query_string):
+    token_key = query_string.decode().split('=')[1]
+    try:
+        token = Token.objects.get(key=token_key)
+        return token.user
+    except Token.DoesNotExist:
+        return None
 
 class RoomConsumer(AsyncWebsocketConsumer): 
     async def connect(self): 
         self.room_name = self.scope['url_route']['kwargs']['room_name'] 
         self.room_group_name = self.room_name
         self.user = await self.get_user(self.scope['query_string'])
-        print(self.user)
- 
- 
-        await self.channel_layer.group_add( 
-                self.room_group_name, 
-                self.channel_name 
-            ) 
-            
-        await self.accept()
+        self.room = await self.get_room(self.room_name)
+
+        await self.get_members()
+        if self.user is not None:
+            await self.channel_layer.group_add( 
+                    self.room_group_name, 
+                    self.channel_name 
+                ) 
+                
+            await self.accept()
+        else:
+            await self.close()
     
     @database_sync_to_async
     def get_user(self, query_string):
@@ -28,32 +41,35 @@ class RoomConsumer(AsyncWebsocketConsumer):
             return token.user
         except Token.DoesNotExist:
             return None
- 
+    
+    @database_sync_to_async
+    def get_room(self, room_name):
+        room = Room.objects.get(name=room_name)
+        self.creator = room.creator
+        return room
+    
+    @database_sync_to_async
+    def get_members(self):
+        room = self.room
+        try:
+            members = Member.objects.filter(room=room).all()
+            print(members)
+        except:
+            print("Нет мемберов")
+
     async def disconnect(self, close_code): 
- 
-        user = self.scope['user'] 
-        is_admin = await self.check_admin(user) 
- 
+  
+        print(self.user)
         await self.channel_layer.group_discard( 
                 self.room_group_name, 
                 self.channel_name 
             ) 
          
-        if is_admin: 
-            await self.close_room(self.room_name) 
- 
-    async def check_admin(self, user): 
-        room_name = self.scope['url_route']['kwargs']['room_name'] 
+
+    @database_sync_to_async 
+    def close_room(self, room_name): 
         try: 
-            room = await sync_to_async(Room.objects.get)(name=room_name) 
-            return user == room.creator 
-        except Room.DoesNotExist: 
-            return False 
-     
-    async def close_room(self, room_name): 
-          
-        try: 
-            room = await sync_to_async(Room.objects.get)(name=room_name) 
+            room = (Room.objects.get)(name=room_name) 
             room.delete() 
         except Room.DoesNotExist: 
             pass 
@@ -64,8 +80,8 @@ class RoomConsumer(AsyncWebsocketConsumer):
 
         signal = text_data_json['signal']
 
-        user = await self.get_user(self.scope['query_string'])
-        #is_admin =  self.check_admin(user)
+        is_admin = self.user == self.creator
+
         print(text_data_json)
         is_admin = True
     
@@ -83,6 +99,8 @@ class RoomConsumer(AsyncWebsocketConsumer):
                     'currentTime': current_time
                 } 
             )
+
+
         if signal == 'url_change':
             url = text_data_json['new_url']
         
@@ -95,6 +113,8 @@ class RoomConsumer(AsyncWebsocketConsumer):
                     'url': url,
                 } 
             )
+
+
         if signal == 'pause':
             await self.channel_layer.group_send( 
                 self.room_group_name, 
@@ -104,8 +124,27 @@ class RoomConsumer(AsyncWebsocketConsumer):
                     'is_admin': is_admin,
                 } 
             )
-    
+        if is_admin and signal == 'disconnect':
+            self.close_all_connections
+        elif signal == 'disconnect':
+            self.close()
+        
+
+        if signal == 'chat':
+            text = text_data_json['text']
+            sender = text_data_json['sender']
+
+            self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'handleChat',
+                    'message': text,
+                    'sender': sender
+                }
+            )
+
  
+    # Video handlers
     async def handlePlay(self, event):
         print('check') 
         signal = event['signal']
@@ -142,12 +181,29 @@ class RoomConsumer(AsyncWebsocketConsumer):
                 'signal': signal,
             }))
     
- 
+    # Chat handle
+            
     async def handleChat(self, event): 
         text = event['message'] 
         sender = event['sender'] 
-         
-        await self.send(text_data=json.dumpss({ 
+        members = await self.get_members()
+        serializer = MembersSerializer(members, many=True)
+
+        await self.send(text_data=json.dumps({ 
             'text': text, 
-            'sender': sender 
+            'sender': sender,
+            'members': serializer 
         }))
+    # Connection close
+            
+    async def close_all_connections(self):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'close_connections',
+            }
+        )
+
+    async def close_connections(self, event):
+        await self.close()
+
