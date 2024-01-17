ed80c81674498538c85c3e7567fc63fc15bea4a6
ky0ufu <superjar@mail.ru>
2024-01-17 23:14:30 +1000

    create docker-compose

--- backend/watch_together_api/settings.py
+++ backend/watch_together_api/settings.py
@@ -76,7 +76,7 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases 
  
 DATABASES = { 
-     "default": {
+      "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
